9b9f0758ac17526c4e3003b442097d641be60872
sookkiong <solnaamu98@gmail.com>
2023-02-28 00:52:10 +0900

    feat: 메인 페이지 모바일 제작

--- src/components/Home/MainSection.tsx
+++ src/components/Home/MainSection.tsx
@@ -5,6 +5,7 @@ import { COLORS } from '../../constants/colors';
 import { ROUTES } from '../../constants/routes';
 import useCountNumber from '../../hooks/useCountNumber';
 import Flyer from '../shared/Flyer';
+import { OAuthButton } from './LoginSection';
 
 const MainSection = () => {
   const navigate = useNavigate();
@@ -13,23 +14,58 @@ const MainSection = () => {
 
   return (
     <Section>
-      <Title>
-        지금까지
-        <br /> 붙여진 전단지 <br />
-        <Title id='count'>{count}</Title>개
-      </Title>
-      <Flyer>
-        <FlyerTitle>
-          나이키 신발
-          <FlyerTitle id='have'>있어요?</FlyerTitle>
-        </FlyerTitle>
-        <Tags>
-          <span>#나이키 에어포스</span>
-          <span>#나이키 한정판 신발</span>
-          <span>#남자 발 사이즈 270</span>
-        </Tags>
-      </Flyer>
-      <Button onClick={goStreet}>전단지골목가기</Button>
+      <Container>
+        <Title>
+          지금까지
+          <br /> 붙여진 전단지 <br />
+          <Title id='count'>{count}</Title>개
+        </Title>
+
+        <Flyer>
+          <FlyerTitle>
+            나이키 신발
+            <FlyerTitle id='have'>있어요?</FlyerTitle>
+          </FlyerTitle>
+          <Tags>
+            <span>#나이키 에어포스</span>
+            <span>#나이키 한정판 신발</span>
+            <span>#남자 발 사이즈 270</span>
+          </Tags>
+        </Flyer>
+
+        <Button onClick={goStreet}>전단지 골목 가기</Button>
+      </Container>
+
+      {/* 모바일 */}
+      <ContainerMob>
+        <Flyer>
+          <FlyerTitle>
+            나이키 신발
+            <FlyerTitle id='have'>있어요?</FlyerTitle>
+          </FlyerTitle>
+          <Tags>
+            <span>#나이키 에어포스</span>
+            <span>#나이키 한정판 신발</span>
+            <span>#남자 발 사이즈 270</span>
+          </Tags>
+        </Flyer>
+
+        <TitleMob>
+          지금까지 붙여진 전단지 <br />
+          <TitleMob id='count'>{count}</TitleMob>개
+        </TitleMob>
+
+        {/* 비로그인 - 로그인 버튼 */}
+        <LoginMob>
+          <OAuthButton id='naver'>네이버로 로그인</OAuthButton>
+          <OAuthButton id='kakao'>카카오로 로그인</OAuthButton>
+        </LoginMob>
+
+        {/* 로그인 - 전단지 골목 가기 버튼 */}
+        <div>
+          <Button onClick={goStreet}>전단지 골목 가기</Button>
+        </div>
+      </ContainerMob>
     </Section>
   );
 };
@@ -37,13 +73,34 @@ const MainSection = () => {
 export default MainSection;
 
 const Section = styled.div`
-  display: flex;
   height: 100vh;
-  align-items: center;
-  justify-content: space-around;
   background: url('/img/flyers.png') no-repeat center center;
   background-size: 90%;
   background-color: #f8f8f8;
+  @media all and (max-width: 767px) {
+    background: url('/img/mainBgMob.png') center top;
+    background-size: 150% auto;
+  }
+`;
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  @media all and (max-width: 767px) {
+    display: none;
+  }
+`;
+const ContainerMob = styled.div`
+  display: none;
+  @media all and (max-width: 767px) {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-top: 70px;
+  }
 `;
 
 const Title = styled.span`
@@ -52,6 +109,30 @@ const Title = styled.span`
   &#count {
     color: ${COLORS.MAIN};
   }
+  @media all and (max-width: 767px) {
+    display: none;
+  }
+`;
+const TitleMob = styled.span`
+  display: none;
+  @media all and (max-width: 767px) {
+    display: inline-block;
+    text-align: center;
+    font-family: 'Black Han Sans', sans-serif;
+    font-size: 2rem;
+    &#count {
+      color: ${COLORS.MAIN};
+    }
+  }
+`;
+
+const LoginMob = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10%;
 `;
 
 const Button = styled.button`
@@ -66,15 +147,23 @@ const Button = styled.button`
   font-size: 17px;
   font-weight: 700;
   cursor: pointer;
+  @media all and (max-width: 767px) {
+    font-weight: 500;
+    margin-top: 30px;
+    display: none;
+  }
 `;
 
 const FlyerTitle = styled.div`
   font-size: 1.7rem;
-  font-weight: 900;
+  font-weight: 700;
   text-align: center;
   line-height: 1.4;
   &#have {
-    font-weight: 700;
+    font-weight: 900;
+  }
+  @media all and (max-width: 767px) {
+    font-size: 1.5rem;
   }
 `;
 
@@ -83,4 +172,7 @@ const Tags = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 15px;
+  @media all and (max-width: 767px) {
+    gap: 10px;
+  }
 `;
