70c80da3d17abb389b4ebfe12d134e867a4ddb1b
truewayy <dldudwls888@gmail.com>
2023-03-03 19:40:01 +0900

    feat: API URL 업데이트

--- src/constants/apiUrls.ts
+++ src/constants/apiUrls.ts
@@ -1,6 +1,40 @@
 export const API_URLS = {
-  AUTH: {},
-  DATA: {
-    IMAGES: '/images',
+  AUTH: {
+    REFRESH: '/auth/token-refresh', // 토큰 리프레시
+  },
+  MAIN: {
+    COUNT: '/users', // 메인 카운트
+    POSTS: '/users/list', // 전단지 리스트
+    TAGS: '/users/list/top', // 태그 리스트
+  },
+  USER: {
+    KAKAO_LOGIN: '/users/kakao-login', // 카카오 로그인
+    NAVER_LOGIN: '/users/naver-login', // 네이버 로그인
+    LOGOUT: '/users/logout', // 로그아웃
+    RESIGN: '/users/resign', // 회원탈퇴
+    PROFILE: '/users/profile', // 프로필
+    MYPAGE: {
+      MARK: '/users/mypage/marks', // 북마크
+      NOTIFICATION: '/users/mypage/notifications', // 최근 알림
+      POST: '/users/mypage/posts', // 내가 붙인 전단지
+      COMMENT: '/users/mypage/comments', // 내가 작성한 댓글
+    },
+  },
+  POST: {
+    REGISTER: '/users/posts', // 전단지 등록
+    DETAIL: (id: string) => `/users/posts/${id}`, // 전단지 상세
+    DELETE: (id: string) => `/users/posts/${id}`, // 전단지 삭제
+    BOOKMARK: (id: string) => `/users/posts/${id}`, // 전단지 북마크
+    GETEDIT: (id: string) => `/users/posts/${id}/edit`, // 전단지 수정 정보 조회
+    EDIT: (id: string) => `/users/posts/${id}/edit`, // 전단지 수정
+    REPORT: (id: string) => `/users/posts/${id}/report`, // 전단지 신고
+  },
+  COMMENT: {
+    REGISTER: (id: string) => `/users/posts/${id}`, // 댓글 작성
+    EDIT: (postId: string, commentId: string) =>
+      `/users/posts/${postId}/${commentId}`, // 댓글 수정
+    DELETE: (postId: string, commentId: string) =>
+      `/users/posts/${postId}/${commentId}`, // 댓글 삭제
+    LIST: (id: string) => `/users/posts/${id}/comments`, // 댓글 리스트
   },
 };
