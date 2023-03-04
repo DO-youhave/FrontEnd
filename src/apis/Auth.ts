3bf3380907b99bcbf1933b4d0a9f70cdd45f28dd
sookkiong <solnaamu98@gmail.com>
2023-03-04 22:34:48 +0900

    refactor: 로그인 실패에 대한 예외처리

--- src/apis/Auth.ts
+++ src/apis/Auth.ts
@@ -4,33 +4,48 @@ import { LoginResponse } from './../interfaces/auth';
 import { setLocalStorage } from './../utils/storage';
 import http from './instance';
 
-export const kakaoLogin = async (code: string) => {
-  try {
-    const { accessToken, refreshToken }: LoginResponse = await http.post(
-      API_URLS.USER.KAKAO_LOGIN,
-      {
-        code,
-      }
-    );
-    setLocalStorage(TOKEN_KEY, accessToken);
-    setLocalStorage(REFRESH_KEY, refreshToken);
-  } catch (error) {
-    console.error(error);
-  }
-};
+interface KakaoLoginProps {
+  code: string;
+}
+interface NaverLoginProps {
+  code: string;
+  state?: string;
+}
 
-export const naverLogin = async (code: string, state: string) => {
+export const naverLogin = async (data: NaverLoginProps) => {
   try {
     const { accessToken, refreshToken }: LoginResponse = await http.post(
       API_URLS.USER.NAVER_LOGIN,
-      {
-        code,
-        state,
-      }
+      data
     );
     setLocalStorage(TOKEN_KEY, accessToken);
     setLocalStorage(REFRESH_KEY, refreshToken);
   } catch (error) {
     console.error(error);
+    return {
+      isLoginFailed: true,
+    };
   }
+  return {
+    isLoginFailed: false,
+  };
+};
+
+export const kakaoLogin = async (data: KakaoLoginProps) => {
+  try {
+    const { accessToken, refreshToken }: LoginResponse = await http.post(
+      API_URLS.USER.KAKAO_LOGIN,
+      data
+    );
+    setLocalStorage(TOKEN_KEY, accessToken);
+    setLocalStorage(REFRESH_KEY, refreshToken);
+  } catch (error) {
+    console.error(error);
+    return {
+      isLoginFailed: true,
+    };
+  }
+  return {
+    isLoginFailed: false,
+  };
 };
