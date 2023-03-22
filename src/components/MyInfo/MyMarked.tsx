c951dec70b099cd8061b429c4872a09b4b981e29
truewayy <dldudwls888@gmail.com>
2023-03-22 18:21:19 +0900

    style: 마이페이지 컨텐츠들 디자인 변경

--- src/components/MyInfo/MyMarked.tsx
+++ src/components/MyInfo/MyMarked.tsx
@@ -1,20 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+
+import { ROUTES } from '../../constants/routes';
 import useGetMarkedFlyer from '../../hooks/useGetMarkedFlyer';
-import Flyer from '../Street/Flyer';
-import { GridBox } from './Recent';
+import Flyer from './Flyer';
+import { Tag, Tags } from './MyPosting';
+import { GridBox, Title } from './Recent';
 
 const MyMarked = () => {
+  const navigate = useNavigate();
   const { flyer } = useGetMarkedFlyer();
   return (
     <GridBox>
-      {flyer?.map(({ postId, title, tags, imgUrl }) => (
+      {flyer?.map(({ postId, title, tags }) => (
         <Flyer
           key={postId}
-          id='myPosting'
-          postId={postId}
-          title={title}
-          tags={tags}
-          imgUrl={imgUrl}
-        />
+          id={postId}
+          onClick={() => navigate(ROUTES.FLYER.DETAIL(postId))}>
+          <Title>{title}</Title>
+          <Tags>
+            {tags.map((tag) => (
+              <Tag key={tag}>#{tag}</Tag>
+            ))}
+          </Tags>
+        </Flyer>
       ))}
     </GridBox>
   );
